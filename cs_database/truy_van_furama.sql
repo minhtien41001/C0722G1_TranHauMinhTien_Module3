ee4204fa6805c8ddf6fdafc91c35761c97979331
minhtien41001 <tranminhtien41001@gmail.com>
2022-10-25 20:10:51 +0700

    update

--- cs_database/truy_van_furama.sql
+++ cs_database/truy_van_furama.sql
@@ -201,6 +201,7 @@ from khach_hang
 where is_delete = 1;
 
 -- task:19. Cập nhật giá cho các dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2020 lên gấp đôi.
+SET SQL_SAFE_UPDATES = 0;
 update dich_vu_di_kem 
 set gia = gia * 2 where ma_dich_vu_di_kem in (
 select * 
@@ -216,7 +217,7 @@ from (select dvdk.ma_dich_vu_di_kem
 select *
 from dich_vu_di_kem;
 
--- task:20. Hiển thị thông tin của tất cả các nhân viên và khách hàng có trong hệ thống, 
+-- task:20. Hiểnư thị thông tin của tất cả các nhân viên và khách hàng có trong hệ thống, 
 -- thông tin hiển thị bao gồm id (ma_nhan_vien, ma_khach_hang), ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi.
 select ma_khach_hang as id, ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi, is_delete
 from khach_hang
@@ -224,11 +225,71 @@ union all
 select ma_nhan_vien as id, ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi, is_delete
 from nhan_vien;
 
+-- task 21:	Tạo khung nhìn có tên là v_nhan_vien để lấy được thông tin của tất cả các nhân viên có địa chỉ là “Hải Châu” 
+-- và đã từng lập hợp đồng cho một hoặc nhiều khách hàng bất kì với ngày lập hợp đồng là “12/12/2019”.
+
+create view v_nhan_vien as
+select nv.ma_nhan_vien, nv.ho_ten, nv.dia_chi, hd.ngay_lam_hop_dong
+from nhan_vien nv
+join hop_dong hd on hd.ma_nhan_vien = nv.ma_nhan_vien
+where nv.dia_chi like '%Hải Châu%' and hd.ngay_lam_hop_dong = '2019-12-12';
+
+select * from v_nhan_vien;
+
+drop view v_nhan_vien;
+
+-- task 22: 22.	Thông qua khung nhìn v_nhan_vien thực hiện cập nhật địa chỉ thành “Liên Chiểu” đối với tất cả các nhân viên được nhìn thấy bởi khung nhìn này.
+update v_nhan_vien set dia_chi = 'Liên Chiểu';
+
+-- task 23:	Tạo Stored Procedure sp_xoa_khach_hang dùng để xóa thông tin của một khách hàng nào đó với ma_khach_hang được truyền vào như là 1 tham số của sp_xoa_khach_hang.
+delimiter //
+create procedure sp_xoa_khach_hang(in id_xoa int)
+begin
+	if id_xoa in (select ma_khach_hang from khach_hang) then
+		update khach_hang set is_delete = 1 where ma_khach_hang = id_xoa;
+	end if;
+end //
+delimiter ;
+
+call sp_xoa_khach_hang(1);
+
+select * from khach_hang;
+
+-- task 24: 24.	Tạo Stored Procedure sp_them_moi_hop_dong dùng để thêm mới vào bảng hop_dong với yêu cầu sp_them_moi_hop_dong phải thực hiện kiểm tra
+-- tính hợp lệ của dữ liệu bổ sung, với nguyên tắc không được trùng khóa chính và đảm bảo toàn vẹn tham chiếu đến các bảng liên quan.
 
 
+-- task 25: 25.	Tạo Trigger có tên tr_xoa_hop_dong khi xóa bản ghi trong bảng hop_dong thì hiển thị tổng số lượng bản ghi còn lại 
+-- có trong bảng hop_dong ra giao diện console của database.
+-- Lưu ý: Đối với MySQL thì sử dụng SIGNAL hoặc ghi log thay cho việc ghi ở console.
+drop table if exists lich_su_xoa_hop_dong;
+create table lich_su_xoa_hop_dong(
+ma_hop_dong_bi_xoa int,
+so_hop_dong_con_lai int,
+ngay_cap_nhat datetime
+);
 
+drop trigger if exists tr_xoa_hop_dong;
+delimiter //
+create trigger tr_xoa_hop_dong
+after update on hop_dong for each row
+begin
+	declare so_luong int;
+	if (old.is_delete <> 0) then
+		signal sqlstate '45000'
+		set message_text = 'Hợp đồng đã bị xóa.';
+		else
+			set @so_luong = (select count(*) from hop_dong where is_delete=0);
+			insert into lich_su_xoa_hop_dong
+			values (old.ma_hop_dong, @so_luong, now());
+	end if;
+end //
+delimiter ;
 
+update hop_dong set is_delete = 1 where ma_hop_dong = 5;
+select * from lich_su_xoa_hop_dong;
 
+select * from hop_dong;
 
 
 
